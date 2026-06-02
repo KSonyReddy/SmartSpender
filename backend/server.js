5cca0f9585e7aa5845b82f8d046c37d77236d28f
Sony Reddy <sonyreddy2006@gmail.com>
2026-06-02 14:48:15 +0530

    d1

--- backend/server.js
+++ backend/server.js
@@ -72,6 +72,8 @@ const allowedOrigins = new Set([
   'http://localhost:8080',
   'http://localhost:3000',
   'http://127.0.0.1:8080',
+
+   'https://smartspender-1-m3j5.onrender.com'
 ]);
 
 function isLocalhostOrigin(origin) {
